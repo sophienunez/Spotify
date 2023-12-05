e22cefb180b9aabed3c3e82dc904f484823eccce
Pipelenisf <98329921+Pipelenisf@users.noreply.github.com>
2023-12-05 16:13:30 -0500

    Add files via upload

--- mobile/data.js
+++ mobile/data.js
@@ -18,3 +18,33 @@ import { getFirestore, collection, addDoc } from "https://www.gstatic.com/fireba
   const app = initializeApp(firebaseConfig);
   // Initialize Cloud Firestore and get a reference to the service
   export const db = getFirestore(app);
+
+  saveData.addEventListener('click', validar);
+  
+  let userNameMobile = document.getElementById("printName")
+  let userMailMobile = document.getElementById("printMail")
+  
+  async function validar(){
+      const elementos = [
+        document.getElementById('name').value,
+        document.getElementById('email').value
+    ]
+    if(elementos.every (e => e.trim() !== '')){
+        return guardar(elementos);
+    }
+}
+
+
+
+
+function guardar(elementos){
+    addDoc(collection(db, "users"),{
+       name: elementos[0],
+       email: elementos[1]
+    })
+
+    userNameMobile.textContent = elementos[0]
+    userMailMobile.textContent = elementos[1]
+    console.log('¡Usuario Registrado!', elementos[0])
+    console.log('¡Usuario Registrado!', elementos[1])
+}
